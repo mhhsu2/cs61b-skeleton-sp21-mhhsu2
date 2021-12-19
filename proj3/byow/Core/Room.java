01685dc4b78751149982ff19695d7de184eab29e
Min-Hsiu Hsu <mhhsu2@illinois.edu>
2021-12-19 21:37:14 +0800

    added Room.buildWall method

--- proj3/byow/Core/Room.java
+++ proj3/byow/Core/Room.java
@@ -12,6 +12,7 @@ public class Room {
     private int height;
     private Position pos;
 
+
     /**
      * Constructs a room with given position and dimensions.
      */
@@ -30,5 +31,52 @@ public class Room {
                 tiles[pos.getX() + dx][pos.getY() + dy] = Tileset.FLOOR;
             }
         }
+
+        buildWall(tiles);
     }
+
+    // Get methods for instance variables
+
+    private int getMaxY() {
+        return pos.getY() + height - 1;
+    }
+    private int getMinY() {
+        return pos.getX();
+    }
+
+    private int getMaxX() {
+        return pos.getX() + width - 1;
+    }
+    private int getMinX() {
+        return pos.getX();
+    }
+
+    // Helper methods
+
+    /**
+     * Builds the top/bottom/left/right walls of a room.
+     */
+    private void buildWall(TETile[][] tiles) {
+        /* Top wall */
+        for (int x = getMinX(); x <= getMaxX(); x += 1) {
+            tiles[x][getMaxY()] = Tileset.WALL;
+        }
+
+        /* Bottom wall */
+        for (int x = getMinX(); x <= getMaxX(); x += 1) {
+            tiles[x][getMinY()] = Tileset.WALL;
+        }
+
+        /* Left wall */
+        for (int y = getMinY(); y <= getMaxY(); y += 1) {
+            tiles[getMinX()][y] = Tileset.WALL;
+        }
+
+        /* Right wall */
+        for (int y = getMinY(); y <= getMaxY(); y += 1) {
+            tiles[getMaxX()][y] = Tileset.WALL;
+        }
+    }
+
+
 }
